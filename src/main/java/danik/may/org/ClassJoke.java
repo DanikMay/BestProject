14540ffe2fa4a45b64e3c2703bbe4b980e6b53df
DanikMay <danikmay33710@gmail.com>
2024-05-20 00:30:55 +0300

    Revert "My new project rpg game"

---
+++ src/main/java/danik/may/org/ClassJoke.java
@@ -0,0 +1,7 @@
+package danik.may.org;
+
+public class ClassJoke {
+    public static void main(String[] args){
+        System.out.println("Если долго есть виноград, можно начать есть изюм");
+    }
+}
